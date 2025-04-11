3580fcf67d52e0490b05e8f71cf3c997e893b8d8
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-04-11 08:47:38 -0700

    Day 1 Code

--- final-project-actual.py
+++ final-project-actual.py
@@ -1,24 +1,41 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
+import scipy.stats as st
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+
 
 
 # Load the dataset
 df = pd.read_csv('f1_pitstops_2018_2024.csv')
-# df = df[df['Constructor'] == 'Red Bull']
 
+# Grab relevant columns
 x_cols = ['Air_Temp_C', 'Track_Temp_C', 'Humidity_%', 'Wind_Speed_KMH']
+x_variables = df[x_cols]
+label = df['AvgPitStopTime']
 
+
+# Clean data
 df = df.dropna(subset=x_cols)
 df = df.dropna(subset=['AvgPitStopTime'])
 
-label = df['AvgPitStopTime']
-print(label.describe())
-x_variables = df[x_cols]
 
+# Describe the data
+print(label.describe())
 print(x_variables.describe())
 
+# Check for normality of the data
+for col in x_cols:
+    plt.figure(figsize=(10, 6))
+    plt.hist(df[col], bins=30, edgecolor='black')
+    plt.title(f'Distribution of {col}')
+    plt.xlabel(col)
+    plt.ylabel('Frequency')
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()
+
+# Check for normality of the label
 plt.figure(figsize=(10, 6))
 plt.hist(label, bins=30, edgecolor='black')
 plt.title('Average Pit Stop Time')
@@ -26,3 +43,7 @@ plt.xlabel('Average Pit Stop Time (seconds)')
 plt.ylabel('Frequency')
 plt.grid(axis='y', alpha=0.75)
 plt.show()
+
+
+print("Correlation Results:")
+print(df[['AvgPitStopTime'] + x_cols].corr())
